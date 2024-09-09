1ecd740fec0d1e1eb1f24117d8e843c024a39ce4
ninjaprogrammer2999 <149578035+ninjaprogrammer2999@users.noreply.github.com>
2024-09-10 00:52:53 +0530

    8. default status by express and how to change the default status✅

--- server.js
+++ server.js
@@ -28,6 +28,7 @@ app.get('/old-page.html', (req, res) => {
     res.redirect('/new-page.html');
 })
 */
+///////////////////////////////////////
 /*we have some issue remember we had an .html extension adder in our node webserver 
  so we need something that will help with this
 👉👉👉express accepts regular expressions we can make use of it****
@@ -36,6 +37,7 @@ app.get('/old-page.html', (req, res) => {
 // when there is no extension we consider it aswell for that regular expresion (.html)? ****
 ////👉👉👉as you have noticed these app. methods will execute downward like a waterfall👈👈//////
 // it's like having some conditionals*** and have a default in this it is 404***
+/*
 app.get('^/$|index(.html)?', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'index.html'));
 })
@@ -49,6 +51,23 @@ app.get('/old-page(.html)?', (req, res) => {
 // default👇
 app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', '404.html'));
+})
+*/
+//////////////////////////////////
+// the 404✅ but the statusCode is 200 which means succesful \
+// express adds status by default✅
+// but here in this default 404 case we need to set it 404✅
+app.get('^/$|index(.html)?', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'index.html')); //status 200 by default✅
+})
+app.get('/new-page(.html)?', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'new-page.html')); //status 200 by default✅
+})
+app.get('old-page(.html)?', (req, res) => {
+    res.redirect(301, 'new-page.html'); // here the status is 302 by default and we need change it 👉👉redirect(301, 'new-page.html);S
+})
+app.get('/*', (req, res) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html')); // so here status is 200 by default we need to change it to 404 by chaining .status(404) 
 })
 app.listen(PORT, () => {
     console.log(`server is running on ${PORT}`);
